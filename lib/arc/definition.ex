8c00a3bedd43cd3c9b23887699682211da5634c1
Sonny Scroggin <scrogson@gmail.com>
2015-12-03 02:03:10 +0100

    Allow users to `use Arc`

--- lib/arc/definition.ex
+++ lib/arc/definition.ex
@@ -1,9 +1,8 @@
 defmodule Arc.Definition do
-  defmacro __using__(options) do
+  defmacro __using__(_opts) do
     quote do
       use Arc.Definition.Versioning
       use Arc.Definition.Storage
-      
       use Arc.Actions.Store
       use Arc.Actions.Url
     end
